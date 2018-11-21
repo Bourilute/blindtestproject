2515ee28f0369439d9dd80260c9902f5a96f0661
Fabien <fabien-du76210@hotmail.fr>
2018-11-21 15:56:46 +0100

    ADD : index.php / quizz.php / style.less / fonts

---
+++ www/quizz.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8" />
+
+	<title>Quiz</title>
+
+	<link rel="stylesheet" href="css/style.css" type="text/css" />
+</head>
+<body>
+	<main class="quizz">
+		<header>
+			<h1><a href="index.php">Blinquizz!</a></h1>
+			<a href="#"><img src="assets/images/avatar.jpg" alt="Mon compte" /></a>
+		</header>
+
+		<section>
+			<p class="question">Question : Trouvez la bonne réponse</p>
+
+			<div class="reponses">
+				<div id="1" class="choix"><p>Réponse 1</p></div>
+				<div id="2" class="choix"><p>Réponse 1</p></div>
+				<div id="3" class="choix"><p>Réponse 1</p></div>
+				<div id="4" class="choix"><p>Réponse 1</p></div>
+			</div>
+		</section>
+
+		<footer>
+			<p>Mentions légales</p>
+		</footer>
+	</main>
+</body>
+</html>
