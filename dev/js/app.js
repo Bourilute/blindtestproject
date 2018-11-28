7805043a8b8ce305b367e8381644b215efdd0ffc
AddSomething <gregoire.protas@gmail.com>
2018-11-28 19:20:42 +0100

    AJOUT : jQuery

--- dev/js/app.js
+++ dev/js/app.js
@@ -1,15 +1,20 @@
+//@prepros-prepend vendor/jquery/jquery-3.3.1.min.js
+
+
 // Initialisation
 var indiceCourantQuestions = 0;
 
 var questions = [ 'Question 1', 'Question 2' ];
 var reponses  = [ ['Réponse 1.A', 'Réponse 1.B', 'Réponse 1.C', 'Réponse 1.D' ],
                   ['Réponse 2.A', 'Réponse 2.B', 'Réponse 2.C', 'Réponse 2.D' ] ];
-var musiques = ['C:\\Users\\Martin Lesueur\\Desktop\\Devoir - LPRO\\Git\\blind test\\daniel_balavoine_lechanteur.mp3', 'C:\\Users\\Martin Lesueur\\Desktop\\Devoir - LPRO\\Git\\blind test\\orelsan_suicide_social.mp3'];
+var musiques = ['assets/music/botw.mp3', 'assets/music/bd.mp3'];
 
-var questionElement = document.getElementById        ( 'question' );
-var choixElements   = document.getElementsByClassName( 'choix'    );
-var questionMusic = document.getElementById          ('music');
-var blocAudio = document.getElementById ('bloc-audio');
+var questionElement = document.getElementById        ( 'question'  );
+var choixElements   = document.getElementsByClassName( 'choix'     );
+var questionMusic   = document.getElementById        ( 'music'     );
+var blocAudio       = document.getElementById        ( 'bloc-audio');
+//var blocControles   = document.
+
 
 changementQuestion(); // On initialise la première question
 window.addEventListener( 'load', initEvents, false );
@@ -26,17 +31,21 @@ function changementQuestion()
 {
 	if( questions.length > indiceCourantQuestions )
 	{
-    blocAudio.removeChild(blocAudio.childNodes[0]);
+	    var lecteurAudio =   '<audio id="test" src="' + musiques[ indiceCourantQuestions ] + '" autoplay></audio>';
+/*
+	  	var controlleurAudio =   '<a class="controls" id="play-button" style="cursor:pointer;"><i class="fas fa-pause fa-2x"></i></a>'
+	  	                       + '<a class="controls" id="stop-button" style="cursor:pointer;"><i class="fas fa-stop fa-2x"></i></a>'
+                               + '<a class="controls" id="pause-button" style="cursor:pointer;"><i class="fas fa-play fa-2x"></i></a>';
+*/
+	    blocAudio.innerHTML = lecteurAudio;
 
-    var lecteurAudio =   '<audio controls>'
-                       + '<source id="music" type="audio/mpeg"'
-                       +  				'src="'+musiques[indiceCourantQuestions]+'"/>'
-  	                   + '</audio>';
-    //blocAudio.appendChild(lecteurAudio);
-    console.log( blocAudio );
-    blocAudio.innerHTML = lecteurAudio ;
+	    var ply = document.getElementById('test');
+		var dur = document.getElementById('test').duration;
 
-    //questionMusic.setAttribute("src", musiques[indiceCourantQuestions]);
+		$('#test').on('timeupdate', function() {
+	    	$('#seekbar').attr("value", this.currentTime / this.duration);
+		});
+
 		questionElement.innerHTML = questions[ indiceCourantQuestions ];
 
 		for( var i = 0; i < choixElements.length; i++ )
@@ -46,7 +55,7 @@ function changementQuestion()
 	}
 	else
 	{
-    questionMusic.innerHTML = '';
+    	questionMusic.innerHTML = '';
 		questionElement.innerHTML = 'Terminé !';
 
 		for( var i = 0; i < choixElements.length; i++ )
