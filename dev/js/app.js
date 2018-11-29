0ef34b23f73b06b7a9718837e05c793c31938dcc
Fabien <fabien.essid@gmail.com>
2018-11-29 20:07:44 +0100

    ADD : Score management

--- dev/js/app.js
+++ dev/js/app.js
@@ -1,11 +1,13 @@
 // Initialisation
 var indiceCourantQuestions = 0;
-
+var totalPoints            = 0;
 
 // Questions, reponses et musiques de la playlist
 var questions = [ 'Question 1', 'Question 2' ];
 var reponses  = [ ['Réponse 1.A', 'Réponse 1.B', 'Réponse 1.C', 'Réponse 1.D' ],
                   ['Réponse 2.A', 'Réponse 2.B', 'Réponse 2.C', 'Réponse 2.D' ] ];
+var score     = [ [1            , 0            , 0            , 0             ],
+                  [0            , 0            , 1            , 0             ] ];
 var musiques  = ['assets/music/botw.mp3', 'assets/music/bd.mp3'];
 
 // Blocs HTML a modifier tout au long du jeu
@@ -60,9 +62,10 @@ function decompte()
 // Quand le joueur clique sur une reponse, cette fonction est appelee
 // Elle actualise la musique, la question et les reponses
 // Quand la derniere question a ete repondue, elle met fin au jeu
-function changementQuestion()
+function changementQuestion( event )
 {
-	console.log( "indiceCourantQuestions : " + indiceCourantQuestions );
+	actualiserScore( event );
+
 	// S'il y a encore des questions
 	if( questions.length > indiceCourantQuestions )
 	{
@@ -93,9 +96,29 @@ function changementQuestion()
 	{
 		// Plus de question? Alors on affiche un message de fin
     	blocAudioElement.innerHTML = "";
-		questionElement.innerHTML = 'Terminé !';
+		questionElement.innerHTML = 'Terminé ! Vous avez obtenu ' + totalPoints + '/' + questions.length;
 
 		for( var i = 0; i < choixElements.length; i++ )
+		{
 			choixElements[ i ].innerHTML = '';
+			choixElements[ i ].style.display = 'none';
+		}
+
+		playerAudio.style.display = 'none';
+	}
+}
+
+function actualiserScore( event )
+{
+	if( indiceCourantQuestions != 0 )
+	{
+		var indice = 0;
+		for( var i = 0; i < reponses[ indiceCourantQuestions-1 ].length; i++ )
+		{
+			if( reponses[ indiceCourantQuestions-1 ][ i ] == event.explicitOriginalTarget.data )
+				indice = i; console.log( "trouvé !" );
+		}
+
+		totalPoints += score[ indiceCourantQuestions-1 ][ indice ];
 	}
 }
